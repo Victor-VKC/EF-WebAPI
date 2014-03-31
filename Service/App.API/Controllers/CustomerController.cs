d8943e89048e9427d465fcc9f473c520ed446fdc
Victor-VKC <victor.vkc@gmail.com>
2014-04-01 01:51:49 +0300

    Test WebAPI. Added simple test app (from Nugets) to App.API

---
+++ Service/App.API/Controllers/CustomerController.cs
@@ -0,0 +1,37 @@
+﻿using System.Web.Http;
+using App.Service;
+using App.Service.DataObjects;
+
+namespace App.API.Controllers
+{
+    public class CustomersController : ApiController
+    {
+        readonly ICustomerService _customerService;
+
+        public CustomersController(ICustomerService customerService)
+        {
+            this._customerService = customerService;
+        }
+
+        /// <summary>
+        /// To validate Customer
+        /// </summary>
+        /// <returns></returns>
+        [System.Web.Http.AcceptVerbs("GET", "POST")]
+        [System.Web.Http.HttpGet]
+        public PersonObject ValidateCustomer(int id, string password)
+        {
+            return _customerService.ValidateCustomer(id, password);
+        }
+
+        /// <summary>
+        /// To Save Customer
+        /// </summary>
+        /// <returns></returns>
+        public int PostCustomer(PersonObject customer)
+        {
+            return _customerService.SaveOrUpdateCustomer(customer);
+        }
+
+    }
+}
